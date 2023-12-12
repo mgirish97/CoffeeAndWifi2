895dedc8601f1f125ec17fda533d22c8f686d60d
Meghana Girish <meghanagirish@hotmail.com>
2023-12-11 18:40:13 -0800

    configure static files

--- mysite/coffeeAndWifi/views.py
+++ mysite/coffeeAndWifi/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('you are at the coffee index.')
+    return render(request, 'coffeeAndWifi/index.html')
 
 # Create your views here.
