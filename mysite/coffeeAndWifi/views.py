c37ec3a6ca87659b7f1631fbebeef73517cf0663
Meghana Girish <meghanagirish@hotmail.com>
2024-01-22 00:34:36 -0800

    Fix Carousel

--- mysite/coffeeAndWifi/views.py
+++ mysite/coffeeAndWifi/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from coffeeAndWifi.models import Cafe
+
+def get_three_cafes():
+    cafe_list = Cafe.objects.all()[:3]
+    return cafe_list
+
 
 def index(request):
-    return render(request, 'coffeeAndWifi/index.html')
+    context = {'cafe_list': get_three_cafes()}
+    return render(request, 'coffeeAndWifi/index.html', context)
 
 # Create your views here.
